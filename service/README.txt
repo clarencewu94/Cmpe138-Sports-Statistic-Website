9735b813d190df431c8892c48d2d5ee1360ca29e
brandonwnguyen <brandonwnguyen@gmail.com>
2017-05-04 15:43:20 -0700

    adding all

---
+++ service/README.txt
@@ -0,0 +1,11 @@
+Brandon Nguyen
+008750768
+brandonwnguyen@gmail.com
+
+
+Instructions
+Open zip file containing the project.
+Open the terminal and change directory to the folder of this readme.
+use the command:
+python app.py
+Open a browser and use 127.0.0.1:5000 as the URL to run.
---
+++ service/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import sqlite3
+
+
+app = Flask(__name__)
+
+# @ is a decorator which is a way to wrap a function and modifying its behavior
+@app.route("/")
+def main():
+		return render_template("frontpage.html")
+
+
+@app.route("/mainmenu")
+def mainmenu():
+	return render_template("mainmenu.html")
+
+@app.route("/signup")
+def signup():
+	return render_template("signin.html")
+
+@app.route("/session")
+def sessionhub():
+	return render_template("SessionHub.html")
+
+@app.route("/basketball")
+def basketball():
+	return render_template("basketball.html")
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
