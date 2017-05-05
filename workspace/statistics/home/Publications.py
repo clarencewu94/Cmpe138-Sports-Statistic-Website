0ba3c8857feb238e8d9b6e23b8affb30f176e751
brandonwnguyen <brandonwnguyen@gmail.com>
2017-05-05 12:54:13 -0700

    basketball class

--- workspace/statistics/home/Publications.py
+++ workspace/statistics/home/Publications.py
@@ -1,41 +1,42 @@
-class User:
+class Basketball:
 
-    def __init__(self, user_name):
-
-        self.user_name = user_name
-
-    def get_user_name(self):
-        return self.user_name
+    def __init__(self, FG, TPT, FT, REB, AST, STL, BLK, TO, PF):
 
 
-class Game:
-    def __init__(self, game_name):
-        
-        self.game_name = game_name
-    
-    def get_game_name(self):
-        return self.game_name
-    
-class Game_Type:
-    def __init__(self, game_type):
-        
-        self.game_type = game_type
-    
-    def get_game_type(self):
-        return self.game_type
-        
-class Stats:
-    def __init__(self, stat_name):
-        
-        self.stat_name = stat_name
-    
-    def get_stat_name(self):
-        return self.stat_name
-        
-class Stat_type:
-    def __init__(self, stat_type):
-        
-        self.stat_type = stat_type
-    
-    def get_stat_type(self):
-        return self.stat_type
+
+        self.FG = FG
+        self.TPT = TPT
+        self.FT = FT
+        self.REB = REB
+        self.AST = AST
+        self.STL = STL
+        self.BLK = BLK
+        self.TO = TO
+        self.PF = PF
+
+    def get_FG(self):
+        return self.FG
+
+    def get_TPT(self):
+        return self.TPT
+
+    def self.FT(self):
+        return self.FT
+
+    def self.REB(self):
+        return self.REB
+
+    def self.AST(self):
+        return self.AST
+
+    def self.STL(self):
+        return self.STL
+
+    def self.BLK(self):
+        return self.BLK
+
+    def self.TO(self):
+        return self.TO
+
+    def self.pf(self):
+        return self.PF
