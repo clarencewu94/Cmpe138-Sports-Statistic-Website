867c4150866404d31179bf6be498c005f292f25a
brandonwnguyen <brandonwnguyen@gmail.com>
2017-05-04 17:38:06 -0700

    tryying to pull

--- workspace/statistics/home/views.py
+++ workspace/statistics/home/views.py
@@ -81,23 +81,73 @@ def add():
     if request.method == "POST"
         add_db = sqlite3.connect('Stats.db')
         add_cursor = add.db.cursor()
-        
-    if request.method == "POST":
-        #insert form information to Books Table and Author Table
-        add_db = sqlite3.connect('database.db')
-        add_cursor = add_db.cursor()
-        year = request.form['year']
-        title = request.form['title']
-        book_title = request.form['book_title']
-        pages = request.form['pages']
-        author_name = request.form['author_name']
-        add_cursor.execute('''INSERT INTO Books(datestamp, Book_Title, Title, Pages, Book_ID)
-                      VALUES(?,?,?,?,?)''', (year,book_title, title, pages, book_id))
-        add_cursor.execute('''INSERT INTO Author(Author_Name, Book_ID)VALUES(?,?)''', 
-                    (author_name, book_id))
-        add_db.commit()
-        success = "Successfully added to database"
-        return render_template('book/add.html', success=success)
-    return render_template('book/add.html')
+        #Basketball
+        Field_Goal = request.form['Field_Goal']
+        Three_Point = request.form['Three_Point']
+        Free_Throws = request.form['Free_Throws']
+        Rebounds = request.form['Rebounds']
+        Assists = request.form['Assists']
+        Steals = request.form['Steals']
+        Blocks = request.form['Blocks']
+        Turnovers = request.form['Turnovers']
+        Personal_Fouls = request.form['Personal_Fouls']
+        #Football
+        Completion = request.form['Completion']
+        Yards = request.form['Yards']
+        Yards_Per_Attempt = request.form['Yards_Per_Attempt']
+        Touchdown = request.form['Touchdown']
+        Interception = request.form['Interception']
+        #Tennis
+        Aces = request.form['Aces']
+        DoubleFaults = request.form['DoubleFaults']
+        1st_serve_in = request.form['1st_serve_in']
+        1st_serve_inWon = request.form['1st_serve_inWon']
+        2nd_serve_Points = request.form['2nd_serve_Points']
+        Wins = request.form['Wins']
+        UnforcedErrors = request.form['UnforcedErrors']
+        NetPointsWon = request.form['NetPointsWon']
+        BreakPointsWon = request.form['BreakPointsWon']
+        #Soccer
+        Shots = request.form['Shots']
+        Assists = request.form['Assists']
+        Goals = request.form['Goals']
+        Save = request.form['Save']
+        Fowls = request.form['Fowls']
+        YellowCards = request.form['YellowCards']
+        RedCards = request.form['RedCards']
+        #Hockey
+        Shots_Attempted = request.form['Shots_Attempted']
+        BlockedbyDefence = request.form['BlockedbyDefence']
+        Shots_Off = request.form['Shots_Off']
+        Pipes = request.form['Pipes']
+        Shots_made = request.form['Shots_made']
+        Goals_Saved = request.form['Goals_Saved']
+        #For Stats
+        db_cursor.execute('''INSERT INTO Stats(  
+            #Basketball
+            Field_Goal, Three_Point, Free_Throws, Rebounds, Assists, Steals, Blocks, Turnovers,Personal_Fouls,
+            #Football
+            Completion, Yards, Yards_Per_Attempt, Touchdown,Interception
+            #Tennis
+            Aces,DoubleFaults, 1st_serve_in, 1st_serve_inWon, 2nd_serve_Points, Wins,UnforcedErrors,NetPointsWon,BreakPointsWon 
+            #Soccer
+            Shots, Assists,Goals,Save,Fowls,YellowCards,RedCards 
+            #Hockey
+            Shots_Attempted,BlockedbyDefence, Shots_Off, Pipes,Shots_made,Goals_Saved) 
+                Values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (  #Basketball
+            Field_Goal, Three_Point, Free_Throws, Rebounds, Assists, Steals, Blocks, Turnovers,Personal_Fouls,
+            #Football
+            Completion, Yards, Yards_Per_Attempt, Touchdown,Interception
+            #Tennis
+            Aces,DoubleFaults, 1st_serve_in, 1st_serve_inWon, 2nd_serve_Points, Wins,UnforcedErrors,NetPointsWon,BreakPointsWon 
+            #Soccer
+            Shots, Assists,Goals,Save,Fowls,YellowCards,RedCards 
+            #Hockey
+            Shots_Attempted,BlockedbyDefence, Shots_Off, Pipes,Shots_made,Goals_Saved))
+            add_db.commit()
+            success = "Successfully added to database"
+               return render_template('SessionHub/SessionHub.html', success=success)
+    return render_template('SessionHub/SessionHub.html')
 
+       
 
