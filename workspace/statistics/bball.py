3dd64174a72570fa77a0250f33810363ba925b3a
Clarence <Clarence>
2017-05-05 23:00:04 -0700

    Resending App.py file

--- workspace/statistics/bball.py
+++ workspace/statistics/bball.py
@@ -40,7 +40,110 @@ class Basketball:
         return self.PF
 
 class Football:
-    def __init__(self,CMP,YDS,TD,INTs,FG,XP)
+    def __init__(self,CMP,YDS,TD,INTs,FG,XP):
+
+        self.CMP = CMP
+        self.YDS = YDS
+        self.TD = TD
+        self.INTs = INTs
+        self.FG = FG 
+        self.XP = XP
+
+    def get_CMP(self):
+        return self.CMP
+    
+    def get_YDS(self):
+        return self.YDS 
+    
+    def get_TD(self):
+        return self.TD
+    
+    def get_INTs(self):
+        return self.INTs
+    
+    def get_FG(self):
+        return self.FG
+    
+class soccer:
+    def __init__(self,shows, saves, offside, fouls, assists, yellow_cards, red_cards):
+
+        self.shows = saves
+        self.saves = offsides
+        self.fouls = fouls
+        self.assists = assists
+        self.yellow_cards = yellow_cards
+        self.red_cards = red_cards
+
+    def get_shows(self):
+        return self.shows
+    
+    def get_saves(self):
+        return self.saves
+    
+    def get_fouls(self):
+        return self.fouls 
+    
+    def get_assists(self):
+        return self.assists
+    
+    def get_yellow_cards(self):
+        return self.yellow_cards
+ 
+    def get_red_cards(self):
+        return self.red_cards
+
+class tennis:
+    def __init__(self, double_faults, aces, serves, net_faults,):
+
+        self.double_faults = double_faults 
+        self.serves = serves
+        self.net_faults = net_faults
+        self.INTs = 
+        self.FG = FG 
+        self.XP = XP
+
+    def get_CMP(self):
+        return self.CMP
+    
+    def get_YDS(self):
+        return self.YDS 
+    
+    def get_TD(self):
+        return self.TD
+    
+    def get_INTs(self):
+        return self.INTSs
+    
+    def get_FG(self):
+        return self.FG
+
+class golf:
+    def __init__(self,CMP,YDS,TD,INTs,FG,XP):
+
+        self.CMP = CMP
+        self.YDS = YDS
+        self.TD = TD
+        self.INTs = INTs
+        self.FG = FG 
+        self.XP = XP
+
+    def get_CMP(self):
+        return self.CMP
+    
+    def get_YDS(self):
+        return self.YDS 
+    
+    def get_TD(self):
+        return self.TD
+    
+    def get_INTs(self):
+        return self.INTSs
+    
+    def get_FG(self):
+        return self.FG
+
+class hockey:
+    def __init__(self,CMP,YDS,TD,INTs,FG,XP):
 
         self.CMP = CMP
         self.YDS = YDS
