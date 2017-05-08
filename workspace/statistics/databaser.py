dddf5c822386817dd8ef8f73732779ad3187fc72
ewn2 ewn2 <ewn2 ewn2>
2017-05-08 15:08:59 -0700

    new new database

--- workspace/statistics/databaser.py
+++ workspace/statistics/databaser.py
@@ -20,7 +20,7 @@ c.execute('''CREATE TABLE tennis
 
 c.execute('''DROP TABLE IF EXISTS golf''')
 c.execute('''CREATE TABLE golf
-             (userid text, game INTEGER PRIMARY KEY AUTOINCREMENT, course_name text, first text, second text, third text, fourth text, fifth text, sixth text, seventh text, eighth text, ninth text, tenth text, eleventh text, twelfth text, thirteenth text, fourteenth text, fifteenth text, sixteenth text, seventeenth text, eighteenth text, FOREIGN KEY (userid) REFERENCES user(userid) ON DELETE CASCADE)''')
+             (userid text, game INTEGER PRIMARY KEY AUTOINCREMENT, course_name text, first text, second text, third text, fourth text, fifth text, sixth text, seventh text, eighth text, ninth text, FOREIGN KEY (userid) REFERENCES user(userid) ON DELETE CASCADE)''')
 
 c.execute('''DROP TABLE IF EXISTS hockey''')
 c.execute('''CREATE TABLE hockey
