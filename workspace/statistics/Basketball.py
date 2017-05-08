3888fc8bc4e15e86466f688292960815aa20ffe0
ewn2 ewn2 <ewn2 ewn2>
2017-05-08 14:27:01 -0700

    changed "shows" to "shots"

--- workspace/statistics/Basketball.py
+++ workspace/statistics/Basketball.py
@@ -88,9 +88,9 @@ class Fdefense:
         return self.interception
     
 class Soccer:
-    def __init__(self,shows, saves, offside, fouls, assists, yellow_cards, red_cards):
+    def __init__(self,shots, saves, offside, fouls, assists, yellow_cards, red_cards):
 
-        self.shows = shows
+        self.shots = shots
         self.saves = saves
         self.offside = offside
         self.fouls = fouls
@@ -98,8 +98,8 @@ class Soccer:
         self.yellow_cards = yellow_cards
         self.red_cards = red_cards
 
-    def get_shows(self):
-        return self.shows
+    def get_shots(self):
+        return self.shots
     
     def get_saves(self):
         return self.saves
